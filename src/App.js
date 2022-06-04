48279c821eda72fdf5e14528458f70e663829850
Kaitlyn Friden <kfriden@verizon.net>
2022-06-04 16:02:53 -0400

    added FaceRecognition folder and file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
 import Background from './components/Animations/ParticleBackground';
 import Navigation from './components/Navigation/navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imagelinkform';
 import Rank from './components/Rank/rank';
+// import Clarifai from 'clarifai';
 import './App.css';
+
+// const app = new Clarifai.App({
+//   apiKey: '2faf493c9735436d97d85ceb9b26f88d'
+// });
 
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  // onButtonSubmit = () => {
+  //   this.setState({imageUrl: this.state.input})
+  //   app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+  //     function(response) {
+  //       // console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+  //     },
+  //     function(err) {
+
+  //     }
+  //   );
+  // }
   render() {
     return(
       <div className="App">
@@ -15,10 +44,10 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         
-        {/* 
-        <FaceRecognition />  */}
+         
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
