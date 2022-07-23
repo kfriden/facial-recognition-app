eeb22c5605b75ea541c1f1660969355596d8cb3c
Kaitlyn Friden <kfriden@verizon.net>
2022-07-23 17:10:29 -0400

    created state for sign in and sign out

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Background from './components/Animations/ParticleBackground';
 import Navigation from './components/Navigation/navigation';
+import SignIn from './components/SignIn/signIn';
+import Register from './components/Register/Register';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Logo from './components/Logo/logo';
 import ImageLinkForm from './components/ImageLinkForm/imagelinkform';
@@ -20,6 +22,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -29,9 +33,18 @@ class App extends Component {
     const width = Number(image.width);
     const height = Number(image.height);
     return {
-      
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
     }
 
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({
+      box: box
+    });
   }
 
   onInputChange = (event) => {
@@ -41,21 +54,39 @@ class App extends Component {
   // onButtonSubmit = () => {
   //   this.setState({imageUrl: this.state.input})
   //   app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-  //   .then(response => this.calculateFaceLocation(response))
+  //   .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
   //   .catch(err => console.log(err));
   // }
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
   render() {
+    const { isSignedIn, imageUrl, route, box } = this.state;
     return(
       <div className="App">
         <Background />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        { this.state.route === 'home' 
+        ? <div> 
+            <Logo />
+            <Rank />
+            <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
+            </div>
+        : (
+          route === 'signin' ? <SignIn onRouteChange={this.onRouteChange}/>
+          : <Register onRouteChange={this.onRouteChange} />
+        )
+
         
-         
-        <FaceRecognition imageUrl={this.state.imageUrl}/>
+            }
       </div>
     );
   }
