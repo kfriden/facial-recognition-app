32d65450fa06aec04fb2e36398255665854452c5
kfriden <kfriden@verizon.net>
2023-05-09 23:12:24 -0400

    small changes but app still breaks

--- src/App.js
+++ src/App.js
@@ -53,9 +53,9 @@ class App extends Component {
     return {
       leftCol: clarifaiFace.left_col * width,
       topRow: clarifaiFace.top_row * height,
-      rightCol: width - (clarifaiFace.right_col * width),
-      bottomRow: height - (clarifaiFace.bottom_row * height)
-    }
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
 
   }
 
@@ -71,25 +71,33 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input})
-    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
-    .then(response => {
-      if (response) {
-        fetch('http://localhost:3000/image', {
-          method: 'put',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-          id: this.state.user.id
-        })
+    this.setState({imageUrl: this.state.input});
+   
+    // HEADS UP! Sometimes the Clarifai Models can be down or not working as they are constantly getting updated.
+    // A good way to check if the model you are using is up, is to check them on the clarifai website. For example,
+    // for the Face Detect Mode: https://www.clarifai.com/models/face-detection
+    // If that isn't working, then that means you will have to wait until their servers are back up. 
+
+    app.models.predict('face-detection', this.state.input)
+      .then(response => {
+        console.log('hi', response)
+        if (response) {
+          fetch('http://localhost:3000/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              id: this.state.user.id
+            })
+          })
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
       })
-      .then(response => response.json())
-      .then(count => {
-        this.setState(Object.assign(this.state.user, { entries: count}))
-      })
-    }
-      this.displayFaceBox(this.calculateFaceLocation(response))
-  })
-    .catch(err => console.log(err));
+      .catch(err => console.log(err));
   }
 
   onRouteChange = (route) => {
